d34dd35e4603877a97cddffded9607bce3e11ba8
himanshu <devliyalhimanshu@gmail.com>
2024-12-17 16:35:35 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-window.addEventListener('scroll', function () {
-    const heroSection = document.querySelector('.hero-section');
-    const scrollPosition = window.scrollY; // Get the scroll position
-    const speed = 0.5; // Adjust this value for slower effect (smaller = slower)
-
-    // Apply background position with a slower scroll speed
-    heroSection.style.backgroundPosition = `center ${scrollPosition * speed}px`;
-});
-
-
-
-
 
 
 window.addEventListener("scroll", function () {
